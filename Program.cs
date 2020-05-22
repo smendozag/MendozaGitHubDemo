c761fcfa72320747222c65a2fb1583baf670f726
SERGIO MENDOZA GOMEZ <smendozagomez@cnm.edu>
2020-05-22 13:17:47 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MendozaGitHubDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
